9afe3c06a6e5f71fc8ee06349cd63880106150b2
jankar-with-tarek <avirus170@gmail.com>
2022-05-22 12:29:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bzokc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const productsCollection = client.db("tool-time").collection('products')
+
+        app.get('/product', async (req, res) => {
+            const products = await productsCollection.find().toArray()
+            res.send(products)
+        })
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
