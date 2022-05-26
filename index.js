e754641dcf1b58d283d918de2731b158fcc75e44
jankar-with-tarek <avirus170@gmail.com>
2022-05-26 20:40:48 +0600

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { send } = require('process');
 require('dotenv').config()
